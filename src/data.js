382df0f3bf17d93a1209446c6e32462c19a2961c
ZeusHunter <govardhanmeena701@gmail.com>
2021-02-18 20:56:21 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        "id": "rec92x18GVY99hQq5",
+        "order": 4,
+        "title": "Primary and Secondary Education",
+        "degree":"SSC",
+        "dates": "July 2006 - May 2015",
+        "duties": [
+        "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+        "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
+        "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism."
+        ],
+        "short": "G.E.H.S.",
+        "institute": "Gurukul English High School",
+        "link": "https://www.gurukulintl.com/",
+        "address": "near ShantaRam Dhaba, Katrap, Badlapur, Maharashtra 421503",
+    },
+    {
+        "id": "rec93x18GVY99hQq5",
+        "order": 5,
+        "title": "Senior Secondary Education",
+        "degree": "HSC",
+        "dates": "July 2015 - May 2017",
+        "duties": [
+        "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+        "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
+        "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism."
+        ],
+        "institute": "New English High School & Jr.College",
+        "short":"N.E.H.S. & Jr.College",
+        "address": "Shri G S Pamnani Marg, Ulhasnagar No 4, Thane - 421004, Netaji Chowk",
+    },
+    {
+        "id": "rec94x18GVY99hQq5",
+        "order": 6,
+        "title": "Computer Science Engineering",
+        "degree": "Integrated B-Tech & M-Tech",
+        "dates": "July 2018 - Present",
+        "duties": [
+        "I'm baby woke mumblecore stumptown enamel pin. Snackwave prism pork belly, blog vape four loko sriracha messenger bag jean shorts DIY bushwick VHS. Banjo post-ironic hella af, palo santo craft beer gluten-free.",
+        "YOLO drinking vinegar chambray pok pok selfies quinoa kinfolk pitchfork street art la croix unicorn DIY. Woke offal jianbing venmo tote bag, palo santo subway tile slow-carb post-ironic pug ugh taxidermy squid.",
+        "Pour-over glossier chambray umami 3 wolf moon. Iceland kale chips asymmetrical craft beer actually forage, biodiesel tattooed fingerstache. Pork belly lomo man braid, portland pitchfork locavore man bun prism."
+        ],
+        "institute": "Indian Institute of Information Technology Design and Manufacturing kancheepuram",
+        "link": "https://iiitdm.ac.in",
+        "short": "IIITDM Kancheepuram",
+        "address": "Melakottaiyur Village,Near Kandigai, Off Vandalur-Kelambakkam Road, Nellikuppam, Chennai, Tamil Nadu 600127",
+    },
+    ]
